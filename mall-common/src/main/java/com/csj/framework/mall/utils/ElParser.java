3c69852655608f7e743bb17a225228f8d05f13e7
weilongyw <247666601@qq.com>
2019-04-04 23:27:20 +0800

    111

---
+++ mall-common/src/main/java/com/csj/framework/mall/utils/ElParser.java
@@ -0,0 +1,26 @@
+package com.csj.framework.mall.utils;
+
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+public class ElParser {
+
+    private static ExpressionParser expressionParser = new SpelExpressionParser();
+
+
+    public static String getKey(String key, String[] paramNames, Object[] args) {
+        Expression expression = expressionParser.parseExpression(key);
+        StandardEvaluationContext context = new StandardEvaluationContext();
+        if (args.length <= 0) {
+            return null;
+        }
+        for (int i = 0; i < args.length; i++) {
+            context.setVariable(paramNames[i], args[i]);
+        }
+        return expression.getValue(context, String.class);
+
+    }
+
+}
